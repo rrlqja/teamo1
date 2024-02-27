b7ca1afb54699660fb7bff52c614f0e31a9a534c
song windows <dkclasltmf@naver.com>
2024-02-27 23:21:59 +0900

    init commit

---
+++ src/main/java/song/teamo1/Teamo1Application.java
@@ -0,0 +1,13 @@
+package song.teamo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teamo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teamo1Application.class, args);
+	}
+
+}
