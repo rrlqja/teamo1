6ad905e3af899b0db96316025ad082c545f6d008
rlqja biz <rrlqja2@naver.com>
2024-11-22 16:44:56 +0900

    feat: InitData 추가

--- src/main/java/song/teamo1/config/InitConfig.java
+++ src/main/java/song/teamo1/config/InitConfig.java
@@ -4,6 +4,8 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import song.teamo1.domain.user.dto.SaveUserDto;
+import song.teamo1.domain.user.service.UserService;
 
 @Slf4j
 @Component
@@ -13,11 +15,17 @@ public class InitConfig {
 
     @PostConstruct
     public void setInit() {
+        initData.init();
     }
 
     @Slf4j
     @Component
     @RequiredArgsConstructor
     private static class InitData {
+        private final UserService userService;
+
+        public void init() {
+            userService.saveUser(new SaveUserDto("1", "1", "n1"));
+        }
     }
 }
